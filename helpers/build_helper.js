0653d2619116017d3ee5644590164eb2d4374b60
tbone <constantx@gmail.com>
2015-02-11 13:29:33 -0800

    remove debug log for build helper

--- helpers/build_helper.js
+++ helpers/build_helper.js
@@ -4,7 +4,6 @@ var block_helpers = {
 
 	"production": function() {
 		return helper_utils.checkArgs(arguments, function(text) {
-			console.log('text', text);
 			if(process.env.NODE_ENV === "development") {
 				return "";
 			}
